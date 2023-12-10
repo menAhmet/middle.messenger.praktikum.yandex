60e479106ddcb4e3aa1aa4b15f69682d47e3783e
Ahmet <berdiyew707@gmail.com>
2023-12-10 22:48:43 +0300

    fix: scss refactoring

--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,6 @@ export default defineConfig({
 	plugins: [handlebars()],
 	build: {
 		outDir: 'dist',
-		//rollupOptions: {
-		//	input: {
-		//		index: resolve(__dirname, 'index.html'),
-		//		signin: resolve(__dirname, './src/pages/signIn/signIn.html'),
-		//		signup: resolve(__dirname, './src/pages/signUp/signUp.html'),
-		//		chat: resolve(__dirname, './src/pages/chat/chat.html'),
-		//		profile: resolve(__dirname, './src/pages/profile/profile.html'),
-		//		profileEdit: resolve(__dirname, './src/pages/profile/profileEdit.html'),
-		//		profilePasswordEdit: resolve(
-		//			__dirname,
-		//			'./src/pages/profile/profilePasswordEdit.html'
-		//		),
-		//	},
-		//},
 	},
 	server: {
 		port: 3000,
